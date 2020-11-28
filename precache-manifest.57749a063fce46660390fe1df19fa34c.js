0eae19659d28f4b2ebc927480db5101db964735c
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-11-28 10:49:21 +0100

    Updates

---
+++ precache-manifest.57749a063fce46660390fe1df19fa34c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c0161d9b383fb41dc9697686549e7a",
+    "url": "/sidebar-and-modal/index.html"
+  },
+  {
+    "revision": "8d94aa41ef6861b65347",
+    "url": "/sidebar-and-modal/static/css/main.e0f79945.chunk.css"
+  },
+  {
+    "revision": "636d57baf1ad64df2cee",
+    "url": "/sidebar-and-modal/static/js/2.91e1b895.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sidebar-and-modal/static/js/2.91e1b895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d94aa41ef6861b65347",
+    "url": "/sidebar-and-modal/static/js/main.5d6a0de8.chunk.js"
+  },
+  {
+    "revision": "e39dc0dae451c0fc3acf",
+    "url": "/sidebar-and-modal/static/js/runtime-main.8c999d0b.js"
+  },
+  {
+    "revision": "2bb7da653fb63433560ef10511c9ae9d",
+    "url": "/sidebar-and-modal/static/media/logo.2bb7da65.svg"
+  }
+]);
